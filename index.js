e3a3b4d8b58724defba9f5526631b44d4ca47a3c
huffmanks <huffmanks2@gmail.com>
2022-06-24 19:29:02 -0400

    audio player and styles

--- index.js
+++ index.js
@@ -1,42 +1,155 @@
 const buildings = Array.from(document.querySelectorAll('[data-building]'))
 const tabs = Array.from(document.querySelectorAll('[data-building-tab]'))
 
+const calculateTime = (secs) => {
+    const minutes = Math.floor(secs / 60)
+    const seconds = Math.floor(secs % 60)
+    const returnedSeconds = seconds < 10 ? `0${seconds}` : `${seconds}`
+    return `${minutes}:${returnedSeconds}`
+}
+
+const displayDuration = (audioDurationEl, audio) => {
+    audioDurationEl.textContent = calculateTime(audio.duration)
+}
+
+const setAudioTrackMax = (audioTrackEl, audio) => {
+    audioTrackEl.max = Math.floor(audio.duration)
+}
+
+const createVideo = (image, mediaOptions, media) => {
+    image.classList.toggle('hide')
+    mediaOptions.classList.toggle('hide')
+
+    const video = document.createElement('video')
+
+    video.src = './assets/videos/draft.mp4'
+    video.poster = './assets/images/draft.jpg'
+
+    video.autoplay = true
+    video.controls = true
+    video.muted = false
+
+    media.appendChild(video)
+}
+
+const createAudio = (mediaOptions, audioPlayer, audioDurationEl, audioTrackEl, media) => {
+    mediaOptions.classList.toggle('hide')
+    audioPlayer.classList.toggle('hide')
+
+    const audio = document.createElement('audio')
+
+    audio.src = './assets/audio/draft.mp3'
+
+    audio.autoplay = true
+    audio.muted = false
+
+    media.appendChild(audio)
+
+    if (audio.readyState > 0) {
+        displayDuration(audioDurationEl, audio)
+        setAudioTrackMax(audioTrackEl, audio)
+    } else {
+        audio.addEventListener('loadedmetadata', () => {
+            displayDuration(audioDurationEl, audio)
+            setAudioTrackMax(audioTrackEl, audio)
+        })
+    }
+}
+
 buildings.map((building) => {
     const media = building.querySelector('[data-media]')
     const image = media.querySelector('img')
+    let audioEl
 
-    const overlay = building.querySelector('[data-overlay]')
-    const playBtn = building.querySelector('[data-play]')
+    const mediaOptions = building.querySelector('[data-media-options]')
+    const watchBtn = building.querySelector('[data-watch]')
     const listenBtn = building.querySelector('[data-listen]')
 
-    playBtn.addEventListener('click', () => {
-        image.classList.toggle('hide')
-        overlay.classList.toggle('hide')
+    const audioPlayer = building.querySelector('[data-audio-player]')
+    const audioPlayBtn = audioPlayer.querySelector('[data-audio-play]')
+    const audioPlayIcon = audioPlayer.querySelector('[data-audio-play] img')
+    const audioVolumeBtn = audioPlayer.querySelector('[data-audio-volume]')
+    const audioVolumeIcon = audioPlayer.querySelector('[data-audio-volume] img')
 
-        const video = document.createElement('video')
+    const audioTimeEl = audioPlayer.querySelector('[data-audio-time]')
+    const audioDurationEl = audioPlayer.querySelector('[data-audio-duration]')
 
-        video.src = './assets/videos/draft.mp4'
-        video.poster = './assets/images/draft.jpg'
+    const audioTimelineEl = audioPlayer.querySelector('.audio-timeline')
+    const audioTrackEl = audioPlayer.querySelector('[data-audio-track]')
 
-        video.autoplay = true
-        video.controls = true
-        video.muted = false
+    watchBtn.addEventListener('click', () => {
+        createVideo(image, mediaOptions, media)
 
-        media.appendChild(video)
+        const videoEl = media.querySelector('video')
+
+        videoEl.addEventListener('ended', () => {
+            videoEl.remove()
+
+            image.classList.toggle('hide')
+            mediaOptions.classList.toggle('hide')
+        })
     })
 
     listenBtn.addEventListener('click', () => {
-        overlay.classList.toggle('hide')
+        createAudio(mediaOptions, audioPlayer, audioDurationEl, audioTrackEl, media)
 
-        const audio = document.createElement('audio')
+        audioEl = media.querySelector('audio')
 
-        audio.src = './assets/audio/draft.mp3'
+        audioEl.addEventListener('timeupdate', () => {
+            updateAudioPlayer()
+        })
 
-        audio.autoplay = true
-        audio.controls = true
-        audio.muted = false
+        audioEl.addEventListener('ended', () => {
+            audioEl.remove()
 
-        media.appendChild(audio)
+            mediaOptions.classList.toggle('hide')
+            audioPlayer.classList.toggle('hide')
+
+            // audioEl.currentTime = 0
+            // audioPlayIcon.src = './assets/icons/controls-play.png'
+
+            // updateAudioPlayer()
+        })
+    })
+
+    const updateAudioPlayer = () => {
+        audioTrackEl.value = Math.floor(audioEl.currentTime)
+        audioTimeEl.textContent = calculateTime(audioTrackEl.value)
+        audioTimelineEl.style.setProperty('--track-before-width', `${(audioTrackEl.value / audioTrackEl.max) * 100}%`)
+    }
+
+    audioPlayBtn.addEventListener('click', () => {
+        if (audioEl.paused) {
+            audioEl.play()
+            audioPlayIcon.src = './assets/icons/controls-pause.png'
+        } else {
+            audioEl.pause()
+            audioPlayIcon.src = './assets/icons/controls-play.png'
+        }
+    })
+
+    audioVolumeBtn.addEventListener('click', () => {
+        if (audioEl.muted) {
+            audioEl.muted = false
+            audioVolumeIcon.src = './assets/icons/controls-volume.png'
+        } else {
+            audioEl.muted = true
+            audioVolumeIcon.src = './assets/icons/controls-volume-mute.png'
+        }
+    })
+
+    audioTrackEl.addEventListener('input', () => {
+        audioTimeEl.textContent = calculateTime(audioTrackEl.value)
+
+        audioTimelineEl.style.setProperty('--track-before-width', (audioTrackEl.value / audioTrackEl.max) * 100 + '%')
+    })
+
+    audioTrackEl.addEventListener('change', () => {
+        audioEl.currentTime = audioTrackEl.value
+
+        if (!audioEl.paused) {
+            updateAudioPlayer()
+        }
     })
 })
 
@@ -44,10 +157,7 @@ tabs.map((tab) => {
     const tabBtns = Array.from(tab.querySelectorAll('[data-building-tab-btn]'))
     const tabEls = Array.from(tab.querySelectorAll('[data-building-tab-content]'))
 
-    console.log(tabBtns)
-
     tabBtns.map((button) => {
-        console.log(button)
         button.addEventListener('click', () => {
             const tabNumber = button.dataset.buildingTabBtn
             const tabContent = tab.querySelector(`[data-building-tab-content="${tabNumber}"]`)
